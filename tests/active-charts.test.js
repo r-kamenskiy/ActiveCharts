9c32cb141cf31b6ec425ee1dac46a3ce0f998342
r-kamenskiy <r@kamenskiy.net>
2016-05-15 02:15:44 +0300

    tests

---
+++ tests/active-charts.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+var activeCharts = require('../dist/active-charts');
+
+describe('active-charts', function(){
+    it('should work!', function(){
+        expect(true).to.be.true;
+    });
+});
